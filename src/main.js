e1ada5d5391e6720fa38bb8e152b564872a96d20
victor <victor@gmail.com>
2025-09-05 15:15:54 +0800

    debug

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-// 仅在开发环境下引入 Eruda
-if (import.meta.env.MODE === 'development') {
-	import('eruda').then(eruda => {
-		eruda.default.init();
-	});
-}
+import eruda from 'eruda'
+eruda.init()
 
 createApp(App).mount('#app')
